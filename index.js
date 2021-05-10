823036948824e8860492a8fc7e9963dd93bf95f8
Paul Fell <swellpf@gmail.com>
2021-05-10 17:28:55 -0400

    Added the form handler code

--- index.js
+++ index.js
@@ -2,23 +2,37 @@ const endPoint = "http://localhost:3000/api/v1/recipes"
 
 document.addEventListener('DOMContentLoaded', () => {
     getRecipes()
-})
+
+    let createRecipeForm = document.querySelector('#create-recipe-form')
+    createRecipeForm.addEventListener('submit', (e) => createFormHandler(e))
+});
     
  function getRecipes(){   
-    fetch(endPoint)
-    .then(response => response.json())
-    .then(recipes => {
-        console.log(recipes);
-        recipes.data.forEach(recipe => {
+     fetch(endPoint)
+     .then(response => response.json())
+     .then(recipes => {
+         recipes.data.forEach(recipe => {
             const recipeMarkup = `
                 <div data-id=${recipe.id}>
                 <h3>${recipe.attributes.name}</h3>
-                <p>${recipe.attributes.category.name}</p>
-                <button data-id'${recipe.id}>edit</button>
+                <p>Category: ${recipe.attributes.category.name}</p>
+                <button data-id'${recipe.id}>View Recipe</button>
                 </div>
                 <br><br>`;
 
         document.querySelector('#recipes-container').innerHTML += recipeMarkup
     })
 })
+ }
+
+ function createFormHandler(e) {
+     e.preventDefault()
+     const nameInput = document.querySelector('#input-name').value
+     const directionsInput = document.querySelector('#input-directions').value
+     const categoryId = parseInt(document.querySelector('#categories').value)
+     postRecipe(nameInput, directionsInput, categoryId)
+ }
+
+ function postRecipe(name, directions, category_id){
+     console.log(name, directions, category_id);
  }
