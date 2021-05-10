e50aa701e7d7c417c73473463f7b5190d67335f3
Paul Fell <swellpf@gmail.com>
2021-05-10 15:35:58 -0400

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("I'm in the index.js file!");
+const endPoint = "http://localhost:3000/api/v1/recipes"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getRecipes()
+})
+    
+ function getRecipes(){   
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(recipes => {
+        console.log(recipes);
+        recipes.data.forEach(recipe => {
+            const recipeMarkup = `
+                <div data-id=${recipe.id}>
+                <h3>${recipe.attributes.name}</h3>
+                <p>${recipe.attributes.category.name}</p>
+                <button data-id'${recipe.id}>edit</button>
+                </div>
+                <br><br>`;
+
+        document.querySelector('#recipes-container').innerHTML += recipeMarkup
+    })
+})
+ }
